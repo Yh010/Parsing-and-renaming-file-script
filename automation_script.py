eca03aed0c688c2d35fc7f0bebd49927831b39ac
Yash Hegde <yash4success@gmail.com>
2023-03-15 13:03:16 +0530

    rearrange file names

--- automation_script.py
+++ automation_script.py
@@ -2,8 +2,17 @@ import os
 os.chdir("path/to/file")
 
 for f in os.listdir():
-    file_name, file_ext = os.path.splitext(f)
+    f_name, f_ext = os.path.splitext(f)
     #print(file_name)
-    f_title, f_course, f_num = file_name.split('-')
-    print(f_title,"\n" ,f_course,"\n" ,f_num)
+    f_title, f_course, f_num = f_name.split('-')
+    #print(f_title,"\n" ,f_course,"\n" ,f_num)
+
+    #remove white space
+    f_title = f_title.strip()
+    f_course = f_course.strip()
+    f_num = f_num.strip()[1:].zfill(2) 
+
+    print('{}-{}-{}{}'.format(f_num,f_course,f_title,f_ext))
+
+
 
