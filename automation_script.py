aedc6fd85a398e80fa88a645b3d5bc287c86e0d8
Yash Hegde <yash4success@gmail.com>
2023-03-15 13:07:00 +0530

    rename files

--- automation_script.py
+++ automation_script.py
@@ -12,7 +12,8 @@ for f in os.listdir():
     f_course = f_course.strip()
     f_num = f_num.strip()[1:].zfill(2) 
 
-    print('{}-{}-{}{}'.format(f_num,f_course,f_title,f_ext))
-
+    #print('{}-{}-{}{}'.format(f_num,f_course,f_title,f_ext))
+    new_name = '{}-{}-{}{}'.format(f_num,f_course,f_title,f_ext)
+    os.rename(f,new_name)
 
 
