3b3e086fc3d06c980fab3bf49450694a88d7be69
Yash Hegde <yash4success@gmail.com>
2023-03-15 12:53:19 +0530

    splitting filename further

--- automation_script.py
+++ automation_script.py
@@ -3,4 +3,7 @@ os.chdir("path/to/file")
 
 for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
-    print(file_name)
+    #print(file_name)
+    f_title, f_course, f_num = file_name.split('-')
+    print(f_title,"\n" ,f_course,"\n" ,f_num)
+
